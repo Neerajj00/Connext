7b20ceacb305d42f82d441e2102ea9cad6f18c83
Neeraj-gupta2005 <neerajKgupta33@gmail.com>
2025-07-18 22:28:33 +0530

    feat: add Clerk authentication middleware and update UI components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,25 @@
+import { Button } from "@/components/ui/button";
+import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      Hello i am starting
+      <SignedOut>
+          <SignInButton mode="modal">
+            <Button>
+              Sign in
+            </Button>
+          </SignInButton>
+        <SignUpButton mode="modal" >
+            <Button variant="secondary">
+              Sign up
+            </Button>
+        </SignUpButton>
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
     </div>
   );
 }
