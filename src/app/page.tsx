c2a657277b0bc39161e17224472878d9deab6d5a
Neeraj-gupta2005 <neerajKgupta33@gmail.com>
2025-07-19 19:31:48 +0530

    mobile and desktop navbar both completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,23 +6,7 @@ import ModeToggle from './../components/ModeToggle';
 export default function Home() {
   return (
     <div>
-      <SignedOut>
-          <SignInButton mode="modal">
-            <Button>
-              Sign in
-            </Button>
-          </SignInButton>
-        <SignUpButton mode="modal" >
-            <Button variant="secondary">
-              Sign up
-            </Button>
-        </SignUpButton>
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-
-      <ModeToggle/>
+        homepage content
     </div>
   );
 }
