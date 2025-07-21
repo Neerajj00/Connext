557339bbb9b3a5736dd1f1d81074eb4cbfe6d8fb
Neeraj-gupta2005 <neerajKgupta33@gmail.com>
2025-07-21 18:58:01 +0530

    made whole profile page

--- src/app/profile/[username]/page.tsx
+++ src/app/profile/[username]/page.tsx
@@ -1,7 +1,33 @@
-import React from "react";
+import { getProfileByUsername, getUserLikedPosts, getUserPosts, isFollowing } from "@/actions/profile.action";
+import { notFound } from "next/navigation";
+import ProfilePageClient from "./ProfilePageClient";
 
-function page({ params }: { params: { username: string } }) {
-  return <div>{params.username}</div>;
+export async function generateMetadata({params}: { params: { username: string } }) {
+  const user = await getProfileByUsername(params.username);
+  if(!user) return;
+
+  return {
+    title: `${user.name ?? user.username}`,
+    description: user.bio || `Check out ${user.username}'s profile.`,
+  }
+}
+
+async function page({ params }: { params: { username: string } }) {
+  const user = await getProfileByUsername(params.username);
+  if(!user) notFound();
+
+  const [posts, likedPosts, isCurrentUserFollowing] = await Promise.all([
+    getUserPosts(user.id),
+    getUserLikedPosts(user.id),
+    isFollowing(user.id)
+  ])
+
+  return <ProfilePageClient 
+    user = {user}
+    posts = {posts}
+    likedPosts = {likedPosts}
+    isFollowing = {isCurrentUserFollowing}
+  />
 }
 
 export default page;
