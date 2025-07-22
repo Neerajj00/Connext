74596d887083c52ddb9dcefeea55c5ac53096d2a
Neeraj-gupta2005 <neerajKgupta33@gmail.com>
2025-07-22 13:57:00 +0530

    foxed the broken profile page in mobile navbar component

--- src/components/MobileNavbar.tsx
+++ src/components/MobileNavbar.tsx
@@ -15,8 +15,10 @@ import { useState } from "react";
 import { useAuth, SignInButton, SignOutButton } from "@clerk/nextjs";
 import { useTheme } from "next-themes";
 import Link from "next/link";
+import { useUser } from "@clerk/nextjs";
 
 function MobileNavbar() {
+  const {user} = useUser();
   const [showMobileMenu, setShowMobileMenu] = useState(false);
   const { isSignedIn } = useAuth();
   const { theme, setTheme } = useTheme();
@@ -61,7 +63,9 @@ function MobileNavbar() {
                   </Link>
                 </Button>
                 <Button variant="ghost" className="flex items-center gap-3 justify-start" asChild>
-                  <Link href="/profile">
+                  <Link href={`/profile/${
+                user?.username ?? user?.emailAddresses[0].emailAddress.split("@")[0]
+              }`}>
                     <UserIcon className="w-4 h-4" />
                     Profile
                   </Link>
