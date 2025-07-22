ac1c8ea65eb52eb0f3140a3ccc8c7c67f27825f7
Neeraj-gupta2005 <neerajKgupta33@gmail.com>
2025-07-22 13:09:57 +0530

    commented console log part which was consoling file details

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -21,8 +21,8 @@ export const ourFileRouter = {
     })
     .onUploadComplete(async ({ metadata, file }) => {
       // This code RUNS ON YOUR SERVER after upload
-      console.log("Upload complete for userId:", metadata.userId);
-      console.log("file details: ", file); // This would log the file object
+      // console.log("Upload complete for userId:", metadata.userId);
+      // console.log("file details: ", file); // This would log the file object
       try {
         return { fileUrl: file.ufsUrl };
       } catch (error) {
